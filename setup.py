fcb6cd7bcbcbda9784b319858dd092c4c5f102cb
Nikolay Stanchev <nikist97@abv.bg>
2019-02-24 17:15:53 +0000

    Fixes versions of external packages in Pipfile and setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,8 @@ setup(name='foldersync',
       description='Client and Server implementations for folder synchronisation',
       author='Nikolay Stanchev',
       author_email='nikist97.ns@gmail.com',
-      packages=find_packages())
+      packages=find_packages(),
+      install_requires=[
+        "twisted==18.9.0",
+        "watchdog==0.9.0"
+      ])
